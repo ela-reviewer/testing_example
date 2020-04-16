743843244808ff94d1edaa66ba70199780f9c63c
ela-reviewer <ela.reviewer2020@gmail.com>
2020-04-16 12:50:08 +0300

    Function seperation

--- banker/banker.py
+++ banker/banker.py
@@ -2,23 +2,54 @@ from __future__ import (absolute_import, division,
                         print_function, unicode_literals)
 from builtins import (bytes, dict, int, list, object, range, str, ascii, chr, hex, input, next, oct, open, pow, round, super, filter, map, zip)
 
-from banker.connection import ConnectionInfo, get_connection
+from banker.connection import get_connection
 
+# Columns of the `info` table
 COLUMNS = [
     'first_name', 'last_name', 'bank_password'
 ]
 
-def main(connection_info, items_to_get, log_file):
-    cursor = get_connection(connection_info).cursor()
-    query = 'SELECT {} FROM Info'.format(','.join(items_to_get))
-    cursor.execute(query)
-    results = []
-    for result in cursor.fetchall():
-        results.append(",".join(result).encode('ascii') +b'\n')
+class FormatException(Exception):
+    """
+    Raised on failure to format a valid query
+    """
+    def __init__(self, reason):
+        super(FormatException, self).__init__(reason)
+        self.reason = reason
 
+    @classmethod
+    def invalid_paramters():
+        return FormatException("Parameters given are not valid!")
+
+    @classmethod
+    def no_paramters():
+        return FormatException("No paramters are given!")
+
+
+def format_query(parameters):
+    """
+    Foramt a query for the given parameters
+    """
+    if not parameters:
+        raise FormatException.no_parameters()
+
+    if not all(map(lambda p: p in COLUMNS, parameters)):
+        raise FormatException.invalid_paramters()
+
+    return 'SELECT {} FROM Info'.format(','.join(parameters))
+
+def parse_result(results):
+    """
+    Parses the result from a query into a list of strings
+    """
+    return [",".join(result).encode('ascii')+b'\n' for result in results]
+
+def main(connection, items_to_get, log_file):
+    """
+    Get the wanted items using the connection, and logs results to log_file
+    """
+    query = format_query(items_to_get)
+    with connection.cursor() as cursor:
+        cursor.execute(query)
+        results = parse_result(cursor.fetchall())
     log_file.writelines(results)
-    print (results)
-
-
-if __name__ == '__main__':
-    main()
